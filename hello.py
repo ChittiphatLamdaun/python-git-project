53754bb5c02400a5c093437999c2d05e4b91679e
ChittiphatLamdaun <chevno2@gmail.com>
2025-08-06 11:02:35 +0700

    fix: resolve merge conflict

--- hello.py
+++ hello.py
@@ -3,12 +3,12 @@ import datetime
 
 def say_hello(name):
     now = datetime.datetime.now()
-    print(f"Goodbye,{name} from {config.APP_NAME}!")
+    print(f"Hello again,{name} from {config.APP_NAME}!")
     print(f"Today is {now.strftime('%Y-%m-%d')}.")
 
 def greet_user():
     name = input("Please enter your name:")
     say_hello(name)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     greet_user()
